14f2ae93d65d8bf579f62ee77220bfce12f11d98
root <root@Ryo-PC.localdomain>
2025-06-17 08:23:19 +0900

    fix-カメラ機能#2

--- test/controllers/progress_controller_test.rb
+++ test/controllers/progress_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class ProgressControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
-    get Progress_index_url
+    get progress_url
     assert_response :success
+  end
+
+  test "should redirect to login when not authenticated" do
+    sign_out @user
+    get progress_url
+    assert_redirected_to new_user_session_path
   end
 end
