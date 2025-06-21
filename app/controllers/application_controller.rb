87f552d703381c98f9f3f7d67c176fe6f15fae17
root <root@Ryo-PC.localdomain>
2025-06-22 05:08:19 +0900

    fix-フッター幅調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,24 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
 
+  helper_method :icon_path, :dynamic_icon_path
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
+  end
+
+  private
+
+  def icon_path(size = nil)
+    # 新しい512x512pxのアイコンファイルを使用
+    asset_path('icon_logo512.png')
+  end
+
+  def dynamic_icon_path(size = '192x192')
+    # 将来的にActive Storageでアイコンを管理する場合の動的リサイズ
+    # 現在は静的ファイルを使用
+    icon_path(size)
   end
 end
