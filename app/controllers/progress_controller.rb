6a73ecfe011f1cba4129353ad043e33cd5d095b1
root <root@Ryo-PC.localdomain>
2025-06-23 11:11:49 +0900

    fix-コントローラー修正

--- app/controllers/progress_controller.rb
+++ app/controllers/progress_controller.rb
@@ -6,12 +6,12 @@ class ProgressController < ApplicationController
 
     # グラフ用のデータを取得（写真の有無を問わない）
     base_query = case @period
-                 when "1w" then current_user.body_records.where("recorded_at >= ?", 1.week.ago)
-                 when "3w" then current_user.body_records.where("recorded_at >= ?", 3.weeks.ago)
-                 when "1m" then current_user.body_records.where("recorded_at >= ?", 1.month.ago)
-                 else # "3m"
+    when "1w" then current_user.body_records.where("recorded_at >= ?", 1.week.ago)
+    when "3w" then current_user.body_records.where("recorded_at >= ?", 3.weeks.ago)
+    when "1m" then current_user.body_records.where("recorded_at >= ?", 1.month.ago)
+    else # "3m"
                    current_user.body_records.where("recorded_at >= ?", 3.months.ago)
-                 end
+    end
 
     @graph_records = base_query.order(:recorded_at)
     @dates = @graph_records.map { |r| r.recorded_at.strftime("%Y-%m-%d") }
@@ -24,7 +24,7 @@ class ProgressController < ApplicationController
                                            .order(:recorded_at)
                                            .select { |r| r.photo.attached? }
     @photos = @body_records_with_photo.map(&:photo)
-    
+
     # Stimulusコントローラー用の全期間写真データ
     @all_photos = current_user.body_records
                               .with_attached_photo
