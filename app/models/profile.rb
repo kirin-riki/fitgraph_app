7ce6af17bf58d51c979dd16300628f1aebfcbf1f
root <root@Ryo-PC.localdomain>
2025-06-29 23:24:09 +0900

    add_比較写真

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,8 +5,8 @@ class Profile < ApplicationRecord
   enum :gender, { man: 0, woman: 1, other: 2 }
   enum :training_intensity, { low: 0, medium: 1, high: 2 }
 
-  validates :height, numericality: { greater_than: 0 }, allow_nil: true
-  validates :target_weight, numericality: { greater_than: 0 }, allow_nil: true
+  validates :height, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
+  validates :target_weight, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
 
   def condition_key
     return nil if gender.nil? || training_intensity.nil?
