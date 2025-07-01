0402b217c5cf1deed4d8514760ba3a47ccf770c6
root <root@Ryo-PC.localdomain>
2025-07-01 20:31:48 +0900

    add_OGP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,27 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'FitGraph',
-      title: '体重・体調管理アプリ',
+      site: "FitGraph",
+      title: "体重・体調管理アプリ",
       reverse: true,
-      charset: 'utf-8',
-      description: 'FitGraphは、毎日の体重や体調をグラフで簡単に記録・管理できるWebアプリです。健康管理やダイエットの習慣化をサポートします。',
-      keywords: '体重,体調,健康管理,ダイエット,グラフ,記録,習慣化',
-      canonical: 'https://fitgraph-app.onrender.com/',
-      separator: '|',
+      charset: "utf-8",
+      description: "FitGraphは、毎日の体重や体調をグラフで簡単に記録・管理できるWebアプリです。健康管理やダイエットの習慣化をサポートします。",
+      keywords: "体重,体調,健康管理,ダイエット,グラフ,記録,習慣化",
+      canonical: "https://fitgraph-app.onrender.com/",
+      separator: "|",
       og: {
         site_name: :site,
         title: :title,
         description: :description,
-        type: 'website',
-        url: 'https://fitgraph-app.onrender.com/',
-        image: image_url('OGP_icon.png'),
-        locale: 'ja_JP'
+        type: "website",
+        url: "https://fitgraph-app.onrender.com/",
+        image: "/OGP_icon.png",
+        locale: "ja_JP"
       },
       twitter: {
-        card: 'summary_large_image',
-        site: '@fitgraph_app',
-        image: image_url('OGP_icon.png')
+        card: "summary_large_image",
+        site: "@fitgraph_app",
+        image: "/OGP_icon.png"
       }
     }
   end
