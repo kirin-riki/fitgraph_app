6ff4c808aea82fea6c376b3af293eba4255bc529
root <root@Ryo-PC.localdomain>
2025-07-02 21:49:00 +0900

    add-googleログイン実装とレイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
-  
+
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:encrypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:encrypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:encrypted_password] }
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     # 以下1行[user = where ...]コメントアウト
     # user = where(provider: auth.provider, uid: auth.uid).first_or_initialize
-    
+
     # 以下の頭に[user = ]を追加
     user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
@@ -25,7 +25,6 @@ class User < ApplicationRecord
       pw = Devise.friendly_token[0, 20]
       user.password = pw
       user.password_confirmation = pw
-
     end
 
     # デバッグ：エラーが出た時にどのようなエラーが出るか確認。
@@ -35,7 +34,7 @@ class User < ApplicationRecord
       Rails.logger.debug "User save failed: #{user.errors.full_messages}"
     end
 
-    return user
+    user
   end
 
   def self.create_unique_string
