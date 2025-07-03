fb9ad3abdaf295694e64e7dee9f92d2f62830af9
root <root@Ryo-PC.localdomain>
2025-07-04 00:24:19 +0900

    line認証

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,10 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2,
                   ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
-                  scope: "email,profile"
+                  {
+                    scope: "email,profile",
+                    redirect_uri: "#{ENV['HOST']}/users/auth/google_oauth2/callback"
+                  }
 
   # LINE認証を追加
   config.omniauth :line,
