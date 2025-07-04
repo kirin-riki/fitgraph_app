33c2709a4ce90653dec73bf87be501e087fd1819
root <root@Ryo-PC.localdomain>
2025-07-04 14:50:52 +0900

    fix-LINE通知

--- lib/line_client.rb
+++ lib/line_client.rb
@@ -5,7 +5,7 @@ class LineClient
     def client
       @client ||= Line::Bot::Client.new do |config|
         config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-        config.channel_access_token = ENV['LINE_CHANNEL_TOKEN']
+        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
       end
     end
 
