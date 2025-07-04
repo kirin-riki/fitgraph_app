66e703fd9c7356c2f8d7ab40567a7ff955d31078
root <root@Ryo-PC.localdomain>
2025-07-04 14:59:33 +0900

    fix-LINE通知

--- lib/line_client.rb
+++ lib/line_client.rb
@@ -2,27 +2,28 @@ require 'line/bot'
 
 class LineClient
   class << self
-    def client
-      @client ||= Line::Bot::Client.new do |config|
-        config.channel_secret = ENV['LINE_CHANNEL_SECRET']
-        config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-      end
+    def parser
+      @parser ||= Line::Bot::V2::WebhookParser.new(channel_secret: ENV['LINE_CHANNEL_SECRET'])
     end
 
-    def validate_signature(body, signature)
-      client.validate_signature(body, signature)
+    def client
+      @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(channel_token: ENV['LINE_CHANNEL_TOKEN'])
+    end
+
+    def parse_events(body, signature)
+      parser.parse(body, signature)
     end
 
     def reply_message(reply_token, messages)
-      client.reply_message(reply_token, messages)
+      client.reply_message(reply_token: reply_token, messages: [messages])
     end
 
     def push_message(user_id, messages)
-      client.push_message(user_id, messages)
+      client.push_message(to: user_id, messages: [messages])
     end
 
     def get_profile(user_id)
-      client.get_profile(user_id)
+      client.get_profile(user_id: user_id)
     end
   end
 end 
