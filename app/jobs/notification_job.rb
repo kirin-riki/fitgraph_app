7f0644121f4bf5887919ab0a010f728047b06df2
root <root@Ryo-PC.localdomain>
2025-07-05 12:32:28 +0900

    fix-通知設定デバッグテスト

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -1,8 +1,6 @@
-class NotificationJob < ApplicationJob
-  queue_as :default
-
-  def perform(user_id)
-    user = User.find(user_id)
-    LineClient.client.push_message(user.line_user_id, { type: 'text', text: 'リマインド通知です！' })
-  end
-end 
+def perform(user_id)
+  Rails.logger.info "=== NotificationJob開始！ user_id: #{user_id} ==="
+  user = User.find(user_id)
+  response = LineClient.client.push_message(user.line_user_id, { type: 'text', text: 'リマインド通知です！' })
+  Rails.logger.info "=== Push送信完了 response: #{response.code} ==="
+end
