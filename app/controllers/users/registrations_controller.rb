7485538912bf1668380ba98a63dde6eaeab1c1b9
root <root@Ryo-PC.localdomain>
2025-07-05 17:04:24 +0900

    addアカウント削除ボタン

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,4 +9,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     resource.update_without_password(params.except("current_password"))
   end
+
+  def destroy
+    # 例：関連ジョブのキャンセル、LINE 連携解除など
+    current_user.cancel_scheduled_jobs
+    current_user.unlink_line_if_needed
+    super   # ← ここで User.destroy が呼ばれる
+  end
 end
