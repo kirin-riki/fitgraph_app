71f691505c630d825fa645f60bf238023ee5919f
root <root@Ryo-PC.localdomain>
2025-07-05 12:06:42 +0900

    fix-通知設定

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -2,6 +2,7 @@ class NotificationJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    LineClient.client.push_message(user_id, { type: 'text', text: 'リマインド通知です！' })
+    user = User.find(user_id)
+    LineClient.client.push_message(user.line_user_id, { type: 'text', text: 'リマインド通知です！' })
   end
 end 
